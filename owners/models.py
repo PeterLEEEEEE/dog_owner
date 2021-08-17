7903b24017b7f2494f05352ef6b5f1978ad9dd5a
peterLEEEEEE <dissgogo@naver.com>
2021-08-17 15:33:02 +0900

    models.py

--- owners/models.py
+++ owners/models.py
@@ -3,5 +3,18 @@ from django.db import models
 # Create your models here.
 
 
-class Owners(models.Model):
-    name = models.Charfield()
+class Owner(models.Model):
+    name = models.Charfield(max_length=45)
+    email = models.CharField(max_length=300)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'owner'
+
+class Dog(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'dog'
