97093a7c82d846233179ee53708df3394bc1cd6f
peterLEEEEEE <dissgogo@naver.com>
2021-08-20 16:00:41 +0900

    dd

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud2',
+        'USER': 'root',
+        'PASSWORD': 'dlangus4862^',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-^5a+uu5fp9fb8x5t=bn8@yo9w##_e)rnlq2$gdxakj^ppl=c*0'
